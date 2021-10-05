9297b5a5d8efe5327e7b0cb0228035c6bad32a8f
Junior-Unix <sonjunior@live.com>
2021-10-05 17:47:01 -0300

    Incluinto no borpo do index.php o conteúdo do recebe.php, barra raiz de diretório no lugar de action'recebe.php'.

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
         <title>Formulário de Contato</title>
     </head>
     <body>
+    <?php
+
+if(isset($_POST['enviar'])){
+    if(empty($_POST['nome'])){
+        echo"Preencha seu nome";
+    }elseif(empty($_POST['email'])){
+        echo "Preencha seu e-mail.";
+    }elseif(empty($_POST['mensagem'])){
+        echo "Preencha sua mensagem";
+    }else{
+            echo "Seu nome é:{$_POST['nome']}<br>";
+            echo "Seu e-mail é:{$_POST['email']}<br>";
+            echo "Sua mensagem é:{$_POST['mensagem']}<br>";
+        }
+ }else{
+    echo "Pela barra de endereço";
+}
+
+?>
         <h1>Formulário de Contato</h1>
-        <form action="recebe.php" method="post">
+        <form action="/" method="post">
             Nome: <input type="text" name="nome"><br>
             E-mail: <input type="text" name="email"><br>
             Mensgem: <br><textarea name="mensagem" rows="8" cols="80"></textarea>
