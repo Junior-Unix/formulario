d49006e2cd408f3a68e68e7e235f2c9aeb459ffb
Junior-Unix <sonjunior@live.com>
2021-10-05 17:37:04 -0300

    Obrigando ao usuário digitar em todos os campos.

--- recebe.php
+++ recebe.php
@@ -1,17 +1,19 @@
 <?php
 
-if(empty($_POST['nome'])){
-    echo "Ela está vazia";
-}else{
-    echo "Não está vazia";
-    echo '<br>';
+if(isset($_POST['enviar'])){
+    if(empty($_POST['nome'])){
+        echo"Preencha seu nome";
+    }elseif(empty($_POST['email'])){
+        echo "Preencha seu e-mail.";
+    }elseif(empty($_POST['mensagem'])){
+        echo "Preencha sua mensagem";
+    }else{
+            echo "Seu nome é:{$_POST['nome']}<br>";
+            echo "Seu e-mail é:{$_POST['email']}<br>";
+            echo "Sua mensagem é:{$_POST['mensagem']}<br>";
+        }
+ }else{
+    echo "Pela barra de endereço";
 }
-
-
-    echo $_POST['nome'];
-    echo'<br>';
-    echo $_POST['email'];
-    echo'<br>';
-    echo $_POST['mensagem'];
 
 ?>
