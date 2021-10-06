868eb78a4d02da42c7c271985df1251002ce2bdf
Junior-Unix <sonjunior@live.com>
2021-10-06 13:25:58 -0300

    Listando banco de dados no navegador, array. OK!

--- gravar.php
+++ gravar.php
@@ -1,11 +1,22 @@
 <?php
-                if(file_exists("dados.txt")){
+/*                 if(file_exists("dados.txt")){
                   $texto = "$nome;$email;$mensagem\n";
                   $fb = fopen("dados.txt", "a+");
                   fwrite( $fb, $texto, strlen($texto));
                   fclose($fb);
                 }else{
                   echo "Não existe.";
-                }
+                } */
 
-              ?>
+/////////////////////////////////////////////////////////
+$conexao = new mysqli("localhost", "root", "0008", "unix");
+
+  if(!$conexao->connect_error){
+      $sql = "INSERT INTO dados (id, nome, email, mensagem, data) VALUES (NULL, '$nome', '$email', '$mensagem', NOW())";
+           if($conexao->query($sql) === TRUE){
+              echo "Inseridos.";
+          }else{
+              echo "Erro ao conectar/inserir dados!";
+      }
+  }
+?>
--- teste.php
+++ teste.php
@@ -1,6 +1,30 @@
 <?php
 
 $conexao = new mysqli("localhost", "root", "0008", "unix");
+    if(!$conexao->connect_error){
+        $sql = "SELECT * FROM dados";
+        $resultado = $conexao->query($sql);
+        if($resultado){
+            while($linha = $resultado->fetch_assoc()){
+                echo 'ID: '.$linha['id'];
+                echo "<br>";
+                echo 'Nome : '.$linha['nome'];
+                echo "<br>";
+                echo 'E-mail : '.$linha['email'];
+                echo "<br>";
+                echo 'mensagem: '.$linha['mensagem'];
+                echo "<br>";
+                echo 'Data/Hora : '.$linha['data'];
+                echo "<br>";
+
+            }
+        }else{
+            echo "Erro ao listar!";
+        }
+    }
+exit;
+
+$conexao = new mysqli("localhost", "root", "0008", "unix");
 
 if(!$conexao->connect_error){
     
